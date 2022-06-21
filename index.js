0e2c9439e1438d6f9dedd9fcae030b7765059b0f
Handy Milliance <milliance23@gmail.com>
2022-06-21 20:51:56 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res) => {
+    console.log('Requisição GET foi chamado')
+    res.send('Olá mundo!')
+})
+app.get('/help', (req,res) => {
+    console.log('Rota de ajuda foi chamada')
+    res.send('Help please!')
+})
+app.get('/:id', (req,res) => {
+    const id = req.params.id
+    res.send(`usuário digitou o ID: ${id}`)
+})
+const verificaIdade = (req, res, next) => {
+    const idade = req.body.idade;
+    if (idade < 16) {
+        res.json({ error: "Você ainda não tem idade para tirar o título de eleitor! "})
+    } else {
+        next();
+    }
+}
+app.post("/", verificaIdade, (req, res) =>  {
+    res.send("Bem-vindo ao portal do Tribunal Superior Eleitoral")
+
+})
+
+app.listen(3000)
